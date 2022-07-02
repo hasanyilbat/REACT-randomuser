aeaf0e3f53d8c58ac149156089ce7bf14f018ef0
Hasan <hasan.yilbat@gmail.com>
2022-07-02 17:42:29 +0300

    randomuser

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useState, useEffect } from "react";
+import "./App.css";
+import icon1 from "./assets/email.svg";
+import icon2 from "./assets/location.svg";
+import icon3 from "./assets/phone.svg";
 
 function App() {
+  const [data, setData] = useState();
+
+  const handleClick = () => {
+    axios
+      .get("https://randomuser.me/api/")
+      .then((res) => setData(res.data.results))
+      .catch((err) => console.log(err));
+
+    console.log(data);
+  };
+
+  useEffect(() => {
+    handleClick();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="container">
+      {card.map(person) => (
+         return(<div className="card">
+        <div>
+          <img src="" alt="" />
+          <h3>isim soyad</h3>
+        </div>
+        <div className="email">
+          <img src={icon1} alt="" className="icon" />
+          <p>email</p>
+        </div>
+        <div className="tel">
+          <img src={icon2} alt="" className="icon" />
+          <p>tel</p>
+        </div>
+        <div className="location">
+          <img src={icon3} alt="" className="icon" />
+          <p>location</p>
+        </div>
+        <div className="age-register">
+          <p></p>
+          <p></p>
+        </div>) }
+      )
+     
+
+      </div>
+      <button onClick={handleClick}>Random User</button>
     </div>
   );
 }
